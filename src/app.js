8d7138a4c9731ff688b79e3a87d467fa681c8009
Niko Hoc <niko.owen88@gmail.com>
2026-05-29 01:06:20 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -5,8 +5,27 @@ const app = express();
 
 const router = require('./routes/index');
 
-// middleware
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000', 
+  process.env.FRONTEND_WEB_URL 
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Akses ditolak oleh CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
